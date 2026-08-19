e760d29a00c007ee54b5697d2186fdc9c750e261
nekrosb <makarov.nikita.mn@gmail.com>
2026-08-19 10:50:37 +0200

    feat: add logging of server

--- src/python_tcp_chat_nekrosb/client/main.py
+++ src/python_tcp_chat_nekrosb/client/main.py
@@ -1,5 +1,6 @@
 def main():
     print("Starting client...")
 
+
 if __name__ == "__main__":
-        main()
+    main()
--- src/python_tcp_chat_nekrosb/server/main.py
+++ src/python_tcp_chat_nekrosb/server/main.py
@@ -1,26 +1,38 @@
-from datetime import datetime
+import logging
 import socket
 import threading
 from rich import print
+from datetime import datetime
+
 
 def main():
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%d-%m-%Y %H:%M:%S",
+        filename="server.log",
+        filemode="a",
+    )
     SERVER_TIMEOUT = 1.0  # seconds
     host = "0.0.0.0"
     port = 12345
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host, port))
-    server.listen(5    )
+    server.listen(5)
 
     stop_event = threading.Event()
 
-    # add logs leiter
+    logging.info(f"Server started on {host}:{port}")
     print(
-    f"[bold green]Server started[/bold green] \n"
-    f"on [cyan]{host}:{port}[/cyan] \n"
-    f"at [yellow]{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}[/yellow]"
-)
+        f"[bold green]Server started[/bold green] \n"
+        f"on [cyan]{host}:{port}[/cyan] \n"
+        f"at [yellow]{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}[/yellow]"
+    )
 
-    console_thread = threading.Thread(target=console_input, args=(stop_event,), daemon=True)
+    console_thread = threading.Thread(
+        target=console_input, args=(stop_event,), daemon=True
+    )
     console_thread.start()
 
     try:
@@ -28,17 +40,20 @@ def main():
             server.settimeout(SERVER_TIMEOUT)
             try:
                 client_socket, addr = server.accept()
-                # add logs leiter
-                print(f"[bold blue]Connection from {addr} has been established.[/bold blue]")
+                logging.info(f"Connection from {addr} has been established.")
+                print(
+                    f"[bold blue]Connection from {addr} has been established.[/bold blue]"
+                )
                 threading.Thread(target=handle_client, args=(client_socket,)).start()
             except socket.timeout:
                 continue
-    except KeyboardInterrupt:
-        print(f"[bold red]Server shutting down.[/bold red] from keyboard interrupt at [yellow]{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}[/yellow]")
+    except KeyboardInterrupt as e:
+        logging.info(f"Server shutting down from keyboard interrupt: {e}")
+        print(
+            f"[bold red]Server shutting down.[/bold red] from keyboard interrupt at [yellow]{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}[/yellow]"
+        )
     finally:
         server.close()
-
-    
 
 
 def handle_client(client_socket):
@@ -47,13 +62,13 @@ def handle_client(client_socket):
             message = client_socket.recv(1024)
             if not message:
                 client_socket.send(b"goodbye")
-                # add logs leiter
+                logging.info("Client disconnected.")
                 print("[bold red]Client disconnected.[/bold red]")
                 break
             print(f"Received message: {message}")
             client_socket.sendall(b"Echo:" + message)
         except ConnectionResetError as e:
-            # add logs leiter
+            logging.info(f"Client disconnected unexpectedly: {e}")
             print("[bold red]Client disconnected unexpectedly.[/bold red] \n", e)
             break
 
@@ -67,7 +82,11 @@ def console_input(stop_event):
 
         if command.lower().strip() == "/exit":
             stop_event.set()
-            print(f"[bold red]Server shutting down correctly.[/bold red] from console input at [yellow]{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}[/yellow]")
+            logging.info("Server shutting down correctly from console input.")
+            print(
+                f"[bold red]Server shutting down correctly.[/bold red] from console input at [yellow]{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}[/yellow]"
+            )
             break
+
 
 main()
